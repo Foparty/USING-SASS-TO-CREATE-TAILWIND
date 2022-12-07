3223ca13121626ef80db40f2efbfee7e79ce70a2
Foparty <103907142+Foparty@users.noreply.github.com>
2022-12-07 10:01:12 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h2>so for some reason that was playing with me</h2>
+    </div>
+  )
+}
+
+export default App
