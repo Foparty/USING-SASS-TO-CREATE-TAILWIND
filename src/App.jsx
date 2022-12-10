4d9cfaeb0e27b7fd2279af37f6fc9b700f346ffb
Foparty <103907142+Foparty@users.noreply.github.com>
2022-12-10 12:24:48 +0000

    added shadows

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,20 @@ function App() {
   return (
     <div className='App'>
       <div className='container'>
-        <div className='grid gap-xs'>
-          <div className='col-md-4 bg-blue-500'>test 6</div>
-          <div className='col-md-4 bg-blue-500'>test 6</div>
-          <div className='col-md-4 bg-blue-500'>test 6</div>
+        <div className='grid gap-xs py-4'>
+          <div className='col-md-4 bg-blue-500 shadow-sm my-4 p-4'>test 6</div>
+          <div className='col-md-4 bg-blue-500 shadow-md my-4 p-4'>test 6</div>
+          <div className='col-md-4 bg-blue-500 shadow-lg my-4 p-4'>test 6</div>
+          <div className='col-md-3 bg-blue-500 shadow-xl my-4 p-4'>test 6</div>
+          <div className='col-md-3 bg-blue-500 shadow-2xl my-4 p-4'>test 6</div>
+          <div className='col-md-3 bg-blue-500 shadow-inner my-4 p-4'>test 6</div>
+          <div className='col-md-3 bg-blue-500 shadow-none my-4 p-4'>test 6</div>
+
+          {/* <div className='col-md-4 bg-blue-500 shadow-md'>test 6</div>
+          <div className='col-md-4 bg-blue-500 shadow-lg'>test 6</div>
           <div className='col-md-8 bg-blue-500'>test 6</div>
           <div className='col-md-4 bg-blue-500'>test 6</div>
-          <div className='col-md-12 bg-blue-500'>test 6</div>
+          <div className='col-md-12 bg-blue-500 shadow-2xl'>test 6</div> */}
         </div>
       </div>
     </div>
