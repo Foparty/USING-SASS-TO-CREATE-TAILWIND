da1cf32b87f46937318143e0b8505503071192ca
Foparty <103907142+Foparty@users.noreply.github.com>
2022-12-10 12:07:00 +0000

    grid system working 25%

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ function App() {
   return (
     <div className='App'>
       <div className='container'>
-        <div className='grid gap-xs w-full justify-center align-center'>
-          <div className='test bg-blue-500'>test 6</div>
-          <div className='test bg-blue-500'>test 6</div>
-          <div className='test bg-blue-500'>test 6</div>
+        <div className='grid gap-xs'>
+          <div className='col-md-4 bg-blue-500'>test 6</div>
+          <div className='col-md-4 bg-blue-500'>test 6</div>
+          <div className='col-md-4 bg-blue-500'>test 6</div>
+          <div className='col-md-8 bg-blue-500'>test 6</div>
+          <div className='col-md-4 bg-blue-500'>test 6</div>
+          <div className='col-md-12 bg-blue-500'>test 6</div>
         </div>
       </div>
     </div>
