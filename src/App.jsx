d338f23d6be26aa8cce90cf0880dabaeb888789a
Foparty <103907142+Foparty@users.noreply.github.com>
2022-12-10 08:57:31 +0000

    added borders and underlines

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
+import { useState } from 'react';
 // import reactLogo from './assets/react.svg'
 
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h2>so for some reason that was playing with me</h2>
+    <div className='App'>
+      <h2 className='underline-md border-xl p-4'>so for some reason that was playing with me</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
