a012ca87f18f3922fa05e95c41f1ba1d5dcd36ec
Foparty <103907142+Foparty@users.noreply.github.com>
2022-12-10 13:37:30 +0000

    final por hoy

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='App'>
       <div className='container'>
-        <div className='grid py-4'>
+        <div className='grid gap-sm py-4'>
           <div className='col-md-9 bg-red-500  my-4 p-4 rounded-l-xl'>test 6</div>
           <div className='col-md-3 bg-blue-500  my-4 p-4 rounded-r-xl'>test 6</div>
           <div className='col-md-4 bg-blue-500 shadow-lg my-4 p-4 rounded-bl-xl'>test 6</div>
@@ -23,29 +23,29 @@ function App() {
           <div className='col-md-12 bg-blue-500 shadow-2xl'>test 6</div> */}
         </div>
 
-        <div className='test-outline'>
-          <h2 className='my-1 py-2 border-xl'>item 1</h2>
-          <h2 className='my-1 py-2 border-xl'>item 2</h2>
-          <h2 className='my-1 py-2 border-xl'>item 3</h2>
-          <h2 className='my-1 py-2 border-xl'>item 4</h2>
-          <h2 className='my-1 py-2 border-xl'>item 5</h2>
-          <h2 className='my-1 py-2 border-xl-yellow-500'>item 6</h2>
-          <h2 className='my-1 py-2 border-xl'>item 7</h2>
-          <h2 className='my-1 py-2 border-xl'>item 8</h2>
-          <h2 className='my-1 py-2 border-xl'>item 9</h2>
-          <h2 className='my-1 py-2 border-xl'>item 10</h2>
+        <div className='test-outline grid gap-sm'>
+          <h2 className='col-md-3 my-1 py-2 border-xl'>item 1</h2>
+          <h2 className='col-md-3 my-1 py-2 border-xl'>item 2</h2>
+          <h2 className='col-md-6 my-1 py-2 border-xl'>item 3</h2>
+          <h2 className='col-md-6 my-1 py-2 border-xl'>item 4</h2>
+          <h2 className='col-md-2 my-1 py-2 border-xl'>item 5</h2>
+          <h2 className='col-md-4 my-1 py-2 border-xl-yellow-500'>item 6</h2>
+          <h2 className='col-sm-4 my-1 py-2 border-xl'>item 7</h2>
+          <h2 className='col-sm-4 my-1 py-2 border-xl'>item 8</h2>
+          <h2 className='col-sm-4 my-1 py-2 border-xl'>item 9</h2>
+          <h2 className='col-md-12 my-1 py-2 border-xl'>item 10</h2>
         </div>
-        <div className='test-outline'>
-          <h2 className='my-1 py-2 outline-xl'>item 1</h2>
-          <h2 className='my-1 py-2 outline-xl-green-300 rounded-base'>item 2</h2>
-          <h2 className='my-1 py-2 outline-xl'>item 3</h2>
-          <h2 className='my-1 py-2 outline-xl'>item 4</h2>
-          <h2 className='my-1 py-2 outline-xl'>item 5</h2>
-          <h2 className='my-1 py-2 outline-xl'>item 6</h2>
-          <h2 className='my-1 py-2 outline-xl'>item 7</h2>
-          <h2 className='my-1 py-2 outline-xl'>item 8</h2>
-          <h2 className='my-1 py-2 outline-xl'>item 9</h2>
-          <h2 className='my-1 py-2 outline-xl'>item 10</h2>
+        <div className='test-outline grid gap-md'>
+          <h2 className='col-sm-4 my-1 py-2 outline-xl'>item 1</h2>
+          <h2 className='col-sm-4 my-1 py-2 outline-xl-green-300 rounded-base'>item 2</h2>
+          <h2 className='col-sm-4 my-1 py-2 outline-xl'>item 3</h2>
+          <h2 className='col-sm-4 my-1 py-2 outline-xl'>item 4</h2>
+          <h2 className='col-sm-4 my-1 py-2 outline-xl'>item 5</h2>
+          <h2 className='col-sm-4 my-1 py-2 outline-xl'>item 6</h2>
+          <h2 className='col-sm-4 my-1 py-2 outline-xl'>item 7</h2>
+          <h2 className='col-sm-4 my-1 py-2 outline-xl'>item 8</h2>
+          <h2 className='col-sm-4 my-1 py-2 outline-xl'>item 9</h2>
+          <h2 className='col-sm-4 my-1 py-2 outline-xl'>item 10</h2>
         </div>
       </div>
     </div>
